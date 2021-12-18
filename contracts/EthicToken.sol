8860d19cb4e9dfeb25f2e877f5dd69be5b84d319
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-18 16:18:36 +0100

    Update Pragma and Add PackageJson

--- contracts/EthicToken.sol
+++ contracts/EthicToken.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.10;
  
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  
+/// @title EthicToken 
+/// @author Lahcen E. Dev
+/// @notice Creation of an ERC20 token, name EthiOnChain and EOC
 contract EthicToken is ERC20 {
 
-    constructor(uint256 _monSupply)  ERC20('EthicOnChain','EOC') {
-        _mint(msg.sender, _monSupply*10**decimals());
-    } 
- 
-
+    /// @notice Explain to an end user what this does
+    /// @dev Initialise the token amount on deployment
+    /// @param _suppliedAmount This is the number of tokens created
+    constructor(uint256 _suppliedAmount)  ERC20("EthicOnChain","EOC") {
+        _mint(msg.sender, _suppliedAmount*10**decimals());
+    }
 }
