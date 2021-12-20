55ca25d521d0d00677b2245099f0e7789939583f
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-20 17:17:15 +0100

    update EthicOnChain file

--- contracts/EthicOnChain.sol
+++ contracts/EthicOnChain.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract EthicOnChain {
     
     struct NPO{
-        bool isNpo;
         string name;
-        string nomination;
+        string denomination;
+        string npoAdress;
         string object;
-        string typeNpo;
-        Projet [] MyProject;
+        string npoType;
+        mapping (uint => Project) Projects;
     }
 
     struct Donor{
@@ -19,23 +19,22 @@ contract EthicOnChain {
         string addressDonor;
     }
 
-    struct Projet{
+    struct Project{
         address ownerNpo;
-        string name;
-        Cause cause;
+        ProjectCause cause;
         string title;
         string description;
         string city;
         uint startDate;
         uint endDate;
-        uint minAmount;
-        uint maxAmount;
-        uint StartCamp;
-        uint DurationCamp;
+        uint32 minAmount;
+        uint32 maxAmount;
+        uint CampaignStartDate;
+        uint CampaignDurationInDays;
     }   
    
 
-    enum Cause{
+    enum ProjectCause{
         LutteContreLapauvreteEtExclusion,
         EnvironnementEtLesAnimaux,
         Education,
@@ -46,8 +45,8 @@ contract EthicOnChain {
     }
 
 
-    enum Status{
-        AddProjectProposal, 
+    enum ProjectStatus{
+        ProjectProposal, 
         WaitingOpening,
         OpenFundRaising,
         CloseFundRaising
@@ -55,14 +54,33 @@ contract EthicOnChain {
 
 
     mapping (address => NPO) public AddressNpo; //Mapping of all NPO 
-    NPO [] public Npos;  
+    mapping ( uint => address) private NpoMap;
+    uint32 NbNpo;
 
-    Projet [] public ProjectProposal;
+    Project [] public ProjectProposal;
     
     event addNewNpo(address _addressNpo,string _name,string _domination);
     event addNewProject(string _name, uint _startDate,uint _endDate,uint _min,uint _max);
 
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+/// @param _name name of Npo
+/// @param _denomination a parameter just like in doxygen (must be followed by parameter name)
+/// @param _npoAddress addres
+/// @param _object a parameter just like in doxygen (must be followed by parameter name)
+/// @param _npoType a parameter just like in doxygen (must be followed by parameter name)
+/// @param _address a parameter just like in doxygen (must be followed by parameter name)
 
+
+    function addNpo(string memory _name,string memory _denomination,string memory _npoAddress,string memory _object,string memory _npoType,address _address) public {
+        AddressNpo[_address].name=_name;
+        AddressNpo[_address].denomination=_denomination;
+        AddressNpo[_address].npoAdress=_npoAddress;
+        AddressNpo[_address].object=_object;
+        AddressNpo[_address].npoType=_npoType;
+        NpoMap[NbNpo]=_address;
+        NbNpo++;
+    }
 
 
 
