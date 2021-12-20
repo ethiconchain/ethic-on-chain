84b1b7c4f765a006eb1b71b71468132c0262ac37
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-20 09:28:43 +0100

    Struct NPO,Project,Donor

--- contracts/EthicOnChain.sol
+++ contracts/EthicOnChain.sol
@@ -2,34 +2,51 @@
 pragma solidity 0.8.10;
  
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-/// @title EthicOnChain.sol
-/// @author Lahcen
-/// @dev Structure for NPO and Project
 contract EthicOnChain {
     
-    struct NPO {
+    struct NPO{
         bool isNpo;
-        string Name;
-        uint Rna;
+        string name;
+        string nomination;
+        string object;
+        string typeNpo;
+        Projet [] MyProject;
+    }
+
+    struct Donor{
+        string name;
+        string surName;
+        string addressDonor;
     }
 
     struct Projet{
-        string Name;
-        string Description;
-        string Target;
-        string Country;
-        string City;
-        uint StartDate;
-        uint EndDate;
-        uint MinAmount;
-        uint MaxAmount;
-        status Status;
-        uint balance;
+        address ownerNpo;
+        string name;
+        Cause cause;
+        string title;
+        string description;
+        string city;
+        uint startDate;
+        uint endDate;
+        uint minAmount;
+        uint maxAmount;
+        uint StartCamp;
+        uint DurationCamp;
     }   
+   
+
+    enum Cause{
+        LutteContreLapauvreteEtExclusion,
+        EnvironnementEtLesAnimaux,
+        Education,
+        ArtEtLaCulture,
+        SanteEtLaRecherche,
+        DroitsDeLHomme,
+        InfrastructureRoutiere
+    }
 
 
-    enum status {
+    enum Status{
         AddProjectProposal, 
         WaitingOpening,
         OpenFundRaising,
@@ -42,52 +59,11 @@ contract EthicOnChain {
 
     Projet [] public ProjectProposal;
     
-    event addNewNpo(address _addressNpo,string _name,uint _Rna);
+    event addNewNpo(address _addressNpo,string _name,string _domination);
     event addNewProject(string _name, uint _startDate,uint _endDate,uint _min,uint _max);
 
 
 
- /// @dev The administrator may add associations 
- /// @param _name Name of Npo
- /// @param _Rna Siret number of Npo
- /// @param _address ERC 20 address of the Npo
-
-    function AddNpo (string memory _name, uint _Rna,address _address) public {
-        Npos.push(NPO(true,_name,_Rna));
-        AddressNpo[_address]=NPO(true,_name,_Rna);
-        emit addNewNpo(_address,_name,_Rna);
-    }
-
-
-
- /// @dev Registered associations can propose a project
- /// @param _nameProject Name of Npo
- /// @param _description Siret number of Npo
- /// @param _country ERC 20 address of the Npo
- /// @param _city Name of Npo
- /// @param _startDate Siret number of Npo
- /// @param _endDate ERC 20 address of the Npo
- /// @param _min Siret number of Npo
- /// @param _max ERC 20 address of the Npo
-
-    function AddProject (
-        string memory _nameProject,
-        string memory _description, 
-        string memory _target,
-        string memory _country,
-        string memory _city,
-        uint _startDate,
-        uint _endDate,
-        uint _min,
-        uint _max
-        ) 
-    public {
-        require(AddressNpo[msg.sender].isNpo==true,"Not registered");
-        Projet memory newProject = Projet(_nameProject,_description,_target,_country,_city,_startDate,_endDate,_min,_max,status.AddProjectProposal,0); 
-        ProjectProposal.push(newProject);
-        emit addNewProject(_nameProject,_startDate,_endDate,_min,_max);
-   
-    }
 
 
 }
