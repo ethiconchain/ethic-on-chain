f6b5a514a480960ae5ee92afb83ba732d1a4a3ab
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-21 03:50:18 +0100

    AddNPO +AddProject+ Update EthicOnChain.sol

--- contracts/EthicOnChain.sol
+++ contracts/EthicOnChain.sol
@@ -16,6 +16,7 @@ contract EthicOnChain {
         string object;
         string npoType;
         mapping (uint => Project) projects;
+        uint nbProject;
     }
 
     struct Donor {
@@ -25,17 +26,16 @@ contract EthicOnChain {
     }
 
     struct Project {
-        address ownerNpo;
         ProjectCause cause;
         string title;
         string description;
         string city;
         uint startDate;
         uint endDate;
-        uint campaignStartDate;
-        uint campaignDurationInDays;
         uint32 minAmount;
         uint32 maxAmount;
+        uint campaignStartDate;
+        uint campaignDurationInDays;
     }      
 
     enum ProjectCause {
@@ -56,30 +56,82 @@ contract EthicOnChain {
     }
 
     mapping (address => NPO) public npoAddresses; //Mapping of all NPO 
-    mapping ( uint => address) private npoMap;
-    uint32 private npoCount;
+    mapping (uint => address) private npoMap;
+    uint32 private nbNpo;
 
-    Project [] public projectProposal;
     
-    event NpoAdded(address _addressNpo, string _name, string _domination);
-    event ProjectAdded(string _name, uint _startDate, uint _endDate, uint _min, uint _max);
+    
+    event addNewNpo(address _addressNpo,string _name);
+    event addNewProject(string _title, uint _startDate,uint _endDate,uint _minAmount,uint _maxAmount);
 
-    /// @dev Add an NPO
-    /// @param _name name of Npo
-    /// @param _denomination name of the NPO
-    /// @param _npoAddress geographical address of the NPO
-    /// @param _object object/purpose of the NPO
-    /// @param _npoType type of the NPO
-    /// @param _address the wallet address of the NPO
-    function addNpo(string memory _name, string memory _denomination, string memory _npoAddress,
-                    string memory _object, string memory _npoType, address _address) public {
-        npoAddresses[_address].name = _name;
-        npoAddresses[_address].denomination = _denomination;
-        npoAddresses[_address].npoAdress = _npoAddress;
-        npoAddresses[_address].object = _object;
-        npoAddresses[_address].npoType = _npoType;
-        npoMap[npoCount] = _address;
-        npoCount++;
+    
+    /// @dev The administrator can add a new NPO
+    /// @param _name Name of the NPO
+    /// @param _denomination Demonination of the NPO
+    /// @param _npoAdress Postal address of the NPO
+    /// @param _object object of NPO
+    /// @param _npoType Type of npo organization
+    /// @param _address the ERC20 address of the npo
+
+    function addNpo(string memory _name,string memory _denomination,string memory _npoAdress,string memory _object,string memory _npoType,address _address) public {
+        npoAddresses[_address].name=_name;
+        npoAddresses[_address].denomination=_denomination;
+        npoAddresses[_address].npoAdress=_npoAdress;
+        npoAddresses[_address].object=_object;
+        npoAddresses[_address].npoType=_npoType;
+        npoMap[nbNpo]=_address;
+        nbNpo++;
+        emit addNewNpo(_address,_name);
+    }
+
+ 
+    /// @dev This function will allow to add a project, the owner will be the one who calls the function. 
+    /// @param _title The title of the project
+    /// @param _description description of the project
+    /// @param _city the city where the project will be located.
+    /// @param _startDate The start date of the project
+    /// @param _endDate The end of the project
+    /// @param _minAmount The minimum price for the project to be valid
+    /// @param _maxAmount The maximum price to make the project a success 
+    /// @param _CampaignStartDate the beginning of the collection of funds
+    /// @param _CampaignDurationInDays the duration of the collection of funds
+
+    function addProject(
+        string memory _title,
+        string memory _description,
+        string memory _city,
+        uint _startDate,
+        uint _endDate,
+        uint32 _minAmount,
+        uint32 _maxAmount,
+        uint _CampaignStartDate,
+        uint _CampaignDurationInDays
+    ) public {
+        
+        npoAddresses[msg.sender].projects[npoAddresses[msg.sender].nbProject]=Project(
+          ProjectCause.LutteContreLapauvreteEtExclusion,
+          _title,
+          _description,
+          _city,
+          _startDate,
+          _endDate,
+          _minAmount,
+          _maxAmount,
+          _CampaignStartDate,
+          _CampaignDurationInDays
+        );
+        npoAddresses[msg.sender].nbProject++;
+        emit addNewProject( _title,  _startDate,_endDate,_minAmount, _maxAmount);
+    }
+    
+
+    /// @dev This function allows to return a project of type Project according to the ERC address of the NPO and the index.
+    /// @param _id index of the project list that the address has
+    /// @param _addressNpo NPO's ERC20 address
+    /// @return Documents the return variables of a contract’s function state variable
+
+    function getProject(uint _id, address _addressNpo) public view returns(Project memory) {
+        return npoAddresses[_addressNpo].projects[_id];
     }
 
 }
