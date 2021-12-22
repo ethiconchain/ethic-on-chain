97a9fcccfc067f0b67ecf4b597844087a79c361a
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-22 14:35:06 +0100

    Update test  +Insertion en base des donateurs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,37 @@
 var EthicToken = artifacts.require("./EthicToken.sol");
-const { BN } = require('@openzeppelin/test-helpers');
+
 module.exports = async function(deployer, _network, accounts) {
+  const owner = accounts[0];
   const Jeff = accounts[1];
   const Bill = accounts[2];
   const Leonardo = accounts[3];
-  const _Amount = new BN(10000000000000000000);
+  const Elon = accounts[4];
 
-  deployer.deploy(EthicToken,"1000000000000",);
-  const TokenEOC = await EthicToken.deployed();
-  await TokenEOC.transfer(accounts[1],_Amount);
-  //await TokenEOC.transfer(Bill,1000000000000);
-  //await TokenEOC.transfer(Leonardo,10000000000000);
 
-  const balance0 = await TokenEOC.balanceOf(accounts[0]);
-  const balance1 = await TokenEOC.balanceOf(accounts[1]);
-  const balance2 = await TokenEOC.balanceOf(accounts[2]);
-  const balance3 = await TokenEOC.balanceOf(accounts[3]);
+  await deployer.deploy(EthicToken,"10000000");
   
+  const TokenEOC = await EthicToken.deployed();
+  await TokenEOC.transfer(Jeff,"100000000000000000000000");
+  await TokenEOC.transfer(Bill,"200000000000000000000000");
+  await TokenEOC.transfer(Leonardo,"300000000000000000000000");
+  await TokenEOC.transfer(Elon,"400000000000000000000000");
+
+  const balance0 = await TokenEOC.balanceOf(owner);
+  const balance1 = await TokenEOC.balanceOf(Jeff);
+  const balance2 = await TokenEOC.balanceOf(Bill);
+  const balance3 = await TokenEOC.balanceOf(Leonardo);
+  const balance4 = await TokenEOC.balanceOf(Elon);
+
+  console.log(owner);
   console.log(Jeff);
   console.log(Bill);
   console.log(Leonardo);
-  console.log(balance0.toString(),"Montant de la balance");
-  console.log(balance1.toString());
+  console.log(Elon);
+
+  console.log(balance0.toString(),"Montant de la balance du owner");
+  console.log(balance1.toString(),"Montant de la balance de Jeff");
+  console.log(balance2.toString(),"Montant de la balance de Bill");
+  console.log(balance3.toString(),"Montant de la balance de Leonardo");
+  console.log(balance4.toString(),"Montant de la balance de Elon");
+
 };
