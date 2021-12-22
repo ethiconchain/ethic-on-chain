c584fba4e98f6eeac47ed4c14f30bb1178737b21
L.EL <89339507+LahcenEL1@users.noreply.github.com>
2021-12-22 12:11:30 +0100

    SP1-05 Insertion en base

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
 var EthicToken = artifacts.require("./EthicToken.sol");
+const { BN } = require('@openzeppelin/test-helpers');
+module.exports = async function(deployer, _network, accounts) {
+  const Jeff = accounts[1];
+  const Bill = accounts[2];
+  const Leonardo = accounts[3];
+  const _Amount = new BN(10000000000000000000);
 
-module.exports = function(deployer) {
-  deployer.deploy(EthicToken,"10000000");
+  deployer.deploy(EthicToken,"1000000000000",);
+  const TokenEOC = await EthicToken.deployed();
+  await TokenEOC.transfer(accounts[1],_Amount);
+  //await TokenEOC.transfer(Bill,1000000000000);
+  //await TokenEOC.transfer(Leonardo,10000000000000);
+
+  const balance0 = await TokenEOC.balanceOf(accounts[0]);
+  const balance1 = await TokenEOC.balanceOf(accounts[1]);
+  const balance2 = await TokenEOC.balanceOf(accounts[2]);
+  const balance3 = await TokenEOC.balanceOf(accounts[3]);
+  
+  console.log(Jeff);
+  console.log(Bill);
+  console.log(Leonardo);
+  console.log(balance0.toString(),"Montant de la balance");
+  console.log(balance1.toString());
 };
