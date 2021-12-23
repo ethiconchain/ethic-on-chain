c12d55faa562e182c698bf0ad388473aa1b4b39a
Sebastien V <bleuplatine@gmail.com>
2021-12-23 16:54:58 +0100

    Update react & react-dom 17, react-scripts 4.0.3 + test deploy Netlify

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var EthicToken = artifacts.require("./EthicToken.sol");
+var EthicOnChain = artifacts.require("./EthicOnChain.sol");
 
-module.exports = async function(deployer, _network, accounts) {
+module.exports = async function (deployer, _network, accounts) {
   const owner = accounts[0];
   const Jeff = accounts[1];
   const Bill = accounts[2];
@@ -8,13 +9,14 @@ module.exports = async function(deployer, _network, accounts) {
   const Elon = accounts[4];
 
 
-  await deployer.deploy(EthicToken,"10000000");
-  
+  await deployer.deploy(EthicOnChain);
+  await deployer.deploy(EthicToken, "10000000");
+
   const TokenEOC = await EthicToken.deployed();
-  await TokenEOC.transfer(Jeff,"100000000000000000000000");
-  await TokenEOC.transfer(Bill,"200000000000000000000000");
-  await TokenEOC.transfer(Leonardo,"300000000000000000000000");
-  await TokenEOC.transfer(Elon,"400000000000000000000000");
+  await TokenEOC.transfer(Jeff, "100000000000000000000000");
+  await TokenEOC.transfer(Bill, "200000000000000000000000");
+  await TokenEOC.transfer(Leonardo, "300000000000000000000000");
+  await TokenEOC.transfer(Elon, "400000000000000000000000");
 
   const balance0 = await TokenEOC.balanceOf(owner);
   const balance1 = await TokenEOC.balanceOf(Jeff);
@@ -28,10 +30,10 @@ module.exports = async function(deployer, _network, accounts) {
   console.log(Leonardo);
   console.log(Elon);
 
-  console.log(balance0.toString(),"Montant de la balance du owner");
-  console.log(balance1.toString(),"Montant de la balance de Jeff");
-  console.log(balance2.toString(),"Montant de la balance de Bill");
-  console.log(balance3.toString(),"Montant de la balance de Leonardo");
-  console.log(balance4.toString(),"Montant de la balance de Elon");
+  console.log(balance0.toString(), "Montant de la balance du owner");
+  console.log(balance1.toString(), "Montant de la balance de Jeff");
+  console.log(balance2.toString(), "Montant de la balance de Bill");
+  console.log(balance3.toString(), "Montant de la balance de Leonardo");
+  console.log(balance4.toString(), "Montant de la balance de Elon");
 
 };
