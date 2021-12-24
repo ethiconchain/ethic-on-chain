19c3ebcb1238626d0fce6a8aaa48f5c039fcae73
Jerome Gauthier <jgauthier@cpaglobal.com>
2021-12-24 07:50:20 +0100

    SP2-00 - déploiement EthicOnChain et ajout des NPOs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,37 +1,85 @@
 var EthicToken = artifacts.require("./EthicToken.sol");
+var EthicOnChain = artifacts.require("./EthicOnChain.sol");
 
 module.exports = async function(deployer, _network, accounts) {
-  const owner = accounts[0];
-  const Jeff = accounts[1];
-  const Bill = accounts[2];
-  const Leonardo = accounts[3];
-  const Elon = accounts[4];
+  const ContractOwner = accounts[0];
+  // les adresses 1 à 4 sont les donateurs
+  const DonorJeff = accounts[1];
+  const DonorBill = accounts[2];
+  const DonorLeonardo = accounts[3];
+  const DonorElon = accounts[4];
+  // les 4 adresses suivantes sont des NPO
+  const NpoFondationMusees = accounts[5];
+  const NpoFondationMichelin = accounts[6];
+  const NpoFondationAssistance = accounts[7];
+  const NpoWfp = accounts[8];
 
-
-  await deployer.deploy(EthicToken,"10000000");
+  ////////////////////////////
+  /// EthicToken et Donors ///
+  ////////////////////////////
+  
+  await deployer.deploy(EthicToken,"1000000"); // on initialise 1 million de tokens
   
   const TokenEOC = await EthicToken.deployed();
-  await TokenEOC.transfer(Jeff,"100000000000000000000000");
-  await TokenEOC.transfer(Bill,"200000000000000000000000");
-  await TokenEOC.transfer(Leonardo,"300000000000000000000000");
-  await TokenEOC.transfer(Elon,"400000000000000000000000");
+  
+  // les Tokens sont ensuite répartis sur 3 donateurs (tant que le contract ne gère pas de swap)
+  await TokenEOC.transfer(DonorJeff,"100000000000000000000000"); // Cent mille à Jeff
+  await TokenEOC.transfer(DonorBill,"200000000000000000000000"); // Deux cents mille à Bill
+  await TokenEOC.transfer(DonorLeonardo,"300000000000000000000000"); // Trois cents mille à Leonardo
+  await TokenEOC.transfer(DonorElon,"400000000000000000000000"); // Quatre cents mille à Elon
 
-  const balance0 = await TokenEOC.balanceOf(owner);
-  const balance1 = await TokenEOC.balanceOf(Jeff);
-  const balance2 = await TokenEOC.balanceOf(Bill);
-  const balance3 = await TokenEOC.balanceOf(Leonardo);
-  const balance4 = await TokenEOC.balanceOf(Elon);
+  const ContractOwnerBalance = await TokenEOC.balanceOf(ContractOwner);
+  const DonorJeffBalance = await TokenEOC.balanceOf(DonorJeff);
+  const DonorBillBalance = await TokenEOC.balanceOf(DonorBill);
+  const DonorLeonardoBalance = await TokenEOC.balanceOf(DonorLeonardo);
+  const DonorElonBalance = await TokenEOC.balanceOf(DonorElon);
 
-  console.log(owner);
-  console.log(Jeff);
-  console.log(Bill);
-  console.log(Leonardo);
-  console.log(Elon);
+  console.log("Adresse du owner du contrat", ContractOwner);
+  console.log("Adresse du donateur Jeff", DonorJeff);
+  console.log("Adresse du donateur Bill", DonorBill);
+  console.log("Adresse du donateur Leonardo", DonorLeonardo);
+  console.log("Adresse du donateur Elon", DonorElon);
 
-  console.log(balance0.toString(),"Montant de la balance du owner");
-  console.log(balance1.toString(),"Montant de la balance de Jeff");
-  console.log(balance2.toString(),"Montant de la balance de Bill");
-  console.log(balance3.toString(),"Montant de la balance de Leonardo");
-  console.log(balance4.toString(),"Montant de la balance de Elon");
+  console.log("Montant de la balance du owner", ContractOwnerBalance.toString());
+  console.log("Montant de la balance de Jeff", DonorJeffBalance.toString());
+  console.log("Montant de la balance de Bill", DonorBillBalance.toString());
+  console.log("Montant de la balance de Leonardo", DonorLeonardoBalance.toString());
+  console.log("Montant de la balance de Elon", DonorElonBalance.toString());
+
+  ////////////////////////////
+  /// EthicOnChain et NPOs ///
+  ////////////////////////////
+  
+  await deployer.deploy(EthicOnChain);
+  
+  const EthicOnChainContract = await EthicOnChain.deployed();
+
+  await EthicOnChainContract.addNpo("La Fondation pour les Musées de France",
+                                    "40 avenue Hoche 75008 Paris",
+                                    "Rénovation de musée",
+                                    "Musée de France",
+                                    NpoFondationMusees);
+  console.log("NPO 'La Fondation pour les Musées de France' créé");
+
+  await EthicOnChainContract.addNpo("Fondation Michelin",
+                                    "23 Pl. des Carmes Dechaux, 63000 Clermont-Ferrand",
+                                    "Mobilité durable, sport et santé, éducation et solidarité, protection de l'environnement, culture et patrimoine",
+                                    "Fondation d’entreprise",
+                                    NpoFondationMichelin);
+  console.log("NPO 'Fondation Michelin' créé");
+
+                                    await EthicOnChainContract.addNpo("Fondation Assistance aux Animaux",
+                                    "77410 Charmentray",
+                                    "Défense des animaux maltraités et abandonnés",
+                                    "Oeuvre ou organisme d’intérêt général",
+                                    NpoFondationAssistance);
+  console.log("NPO 'Fondation Assistance aux Animaux' créé");
+
+                                    await EthicOnChainContract.addNpo("WFP",
+                                    "Via Cesare Giulio Viola, 68, 00148 Rome RM, Italy",
+                                    "Lutte contre la faim dans le monde",
+                                    "Fondation internationale",
+                                    NpoWfp);
+  console.log("NPO 'WFP' créé");
 
 };
