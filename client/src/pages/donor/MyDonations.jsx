ad00df1701c8b024999d41518f1eaa5541e824bb
Jerome Gauthier <jgauthier@cpaglobal.com>
2022-01-14 16:14:01 +0100

    SP4-03 - ajout gestion de la pagination sur Mes Dons

--- client/src/pages/donor/MyDonations.jsx
+++ client/src/pages/donor/MyDonations.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
 import IconButton from '@mui/material/IconButton';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import LastPageIcon from '@mui/icons-material/LastPage';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 
@@ -18,6 +25,8 @@ const MyDonations = (props) => {
   const { web3 } = data
   const [allMyDonations, setAllMyDonations] = useState(null)
   const [allProjects, setAllProjects] = useState(null)
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
     getMyDonations()
@@ -62,13 +71,9 @@ const MyDonations = (props) => {
             }
             break;            
           case "minAmount":
-            returnedValue = tempValue/10**18;
-            break;            
           case "maxAmount":
-            returnedValue = tempValue/10**18;
-            break;            
           case "projectTotalDonations":
-            returnedValue = tempValue/10**18;
+              returnedValue = web3.utils.fromWei(tempValue);
             break;            
           default:
             returnedValue = tempValue;
@@ -90,6 +95,69 @@ const MyDonations = (props) => {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+
+    const handleFirstPageButtonClick = (event) => {
+      onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+      onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+      onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </Box>
+    );
   }
 
   const DonationRow = (props) => {
@@ -163,10 +231,34 @@ const MyDonations = (props) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {allMyDonations.map((donation) => (
-                <DonationRow key={donation.donationId} donation={donation} />
+              {(rowsPerPage > 0
+                ? allMyDonations.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                : allMyDonations
+                ).map((donation) => (
+                  <DonationRow key={donation.donationId} donation={donation} />
               ))}
             </TableBody>
+            <TableFooter >
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={5}
+                  count={allMyDonations.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  labelRowsPerPage="Lignes par page"
+                  SelectProps={{
+                    inputProps: {
+                      'aria-label': 'Lignes par page',
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
         </TableContainer>}
     </>
